2ec12381264b068a0aba2d90efc38679258e4e27
nicoquispe <nicanor.quisp@gmail.com>
2014-03-30 03:57:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var server = http.createServer(app);
 io = io.listen(server);
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
